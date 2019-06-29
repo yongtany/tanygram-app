046485f4b5df07e295589214b81d56ddf9b89677
yongtany <yongn1494@gmail.com>
2019-06-29 18:20:48 +0900

    AuthContext

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { Ionicons } from '@expo/vector-icons';
 import { AppLoading } from 'expo';
 import { Asset } from 'expo-asset';
 import * as Font from 'expo-font';
-import { Text, View, AsyncStorage, TouchableOpacity } from 'react-native';
+import { AsyncStorage  } from 'react-native';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { persistCache } from 'apollo-cache-persist';
 import ApolloClient from 'apollo-boost';
@@ -12,11 +12,13 @@ import { ApolloProvider } from 'react-apollo-hooks';
 
 import apolloClientOptions from './apollo';
 import styles from './styles';
+import NavController from './components/NavController';
+import { AuthProvider } from './AuthContext';
 
 export default function App() {
   const [ loaded, setLoaded ]= useState(false);
   const [ client, setClient ] = useState(null);
-  const [ isLoggedIn, setIsLoggedIn ] = useState(null);
+  const [ isLoggedIn, setIsLoggedIn] = useState(null);
   const preLoad = async () => {
     try {
       await Font.loadAsync({
@@ -33,7 +35,7 @@ export default function App() {
         ...apolloClientOptions
       });
       const isLoggedIn = await AsyncStorage.getItem("isLoggedIn");
-      if(isLoggedIn === null || isLoggedIn === "false") {
+      if(!isLoggedIn || isLoggedIn === "false"){
         setIsLoggedIn(false);
       } else {
         setIsLoggedIn(true);
@@ -49,38 +51,12 @@ export default function App() {
     preLoad();
   }, []);
 
-  const logUserIn = async () => {
-    try {
-      await AsyncStorage.setItem("isLoggedIn", "true");
-      setIsLoggedIn(true);
-    } catch(e) {
-      console.log(e);
-    }
-  }
-
-  const logUserOut = async () => {
-    try {
-      await AsyncStorage.setItem("isLoggedIn", "false");
-      setIsLoggedIn(false);
-    } catch(e) {
-      console.log(e);
-    }
-  }
-
   return loaded && client && isLoggedIn !== null ? (
     <ApolloProvider client={client}>
       <ThemeProvider theme={styles}>
-        <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
-          {isLoggedIn === true ? (
-          <TouchableOpacity onPress={logUserOut}>
-            <Text>Log Out</Text>
-          </TouchableOpacity> 
-          ): (
-          <TouchableOpacity onPress={logUserIn}>
-            <Text>Log In</Text>
-          </TouchableOpacity>
-          )}
-        </View>
+        <AuthProvider isLoggedIn={isLoggedIn}>
+          <NavController />
+        </AuthProvider>
       </ThemeProvider>
     </ApolloProvider>
   ) : (
---
+++ components/NavController.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { useIsLoggedIn, useLogIn, useLogOut } from '../AuthContext';
+
+export default () => {
+    const isLoggedIn = useIsLoggedIn();
+    const logIn = useLogIn();
+    const logOut = useLogOut();
+    return (
+        <View style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+          {isLoggedIn === true ? (
+          <TouchableOpacity onPress={logOut}>
+            <Text>Log Out</Text>
+          </TouchableOpacity> 
+          ): (
+          <TouchableOpacity onPress={logIn}>
+            <Text>Log In</Text>
+          </TouchableOpacity>
+          )}
+        </View>
+    );
+};
