c0bf74f5d40cd9f95a5d7118b6b2916853975515
yongtany <yongn1494@gmail.com>
2019-07-06 18:15:00 +0900

    Apollo Context

--- screens/Tabs/Home.js
+++ screens/Tabs/Home.js
@@ -1,15 +1,47 @@
 import React from "react";
 import styled from "styled-components";
+import { gql } from 'apollo-boost';
 import Loader from "../../components/Loader";
+import { useQuery } from 'react-apollo-hooks';
 
- const View = styled.View`
+const FEED_QUERY = gql`
+  {
+    seeFeed {
+      id
+      location
+      caption
+      user {
+        id
+        avatar
+        username
+      }
+      files {
+        id
+        url
+      }
+      likeCount
+      isLiked
+      comments {
+        id
+        text
+        user {
+          id
+          username
+        }
+      }
+      createdAt
+    }
+  }
+`;
+
+const View = styled.View`
   justify-content: center;
   align-items: center;
   flex: 1;
 `;
 
- export default () => (
-  <View>
-    <Loader />
-  </View>
-);
+export default () => {
+  const { loading, data } = useQuery(FEED_QUERY);
+  console.log(loading, data);
+  return <View>{loading ? <Loader /> : null}</View>
+}
