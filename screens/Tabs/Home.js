13fbc893bd34104369362de6a924a0cdb40f3c8c
yongtany <yongn1494@gmail.com>
2019-07-06 18:29:20 +0900

    ScrollView and RefreshControl

--- screens/Tabs/Home.js
+++ screens/Tabs/Home.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { ScrollView, RefreshControl } from 'react-native';
 import styled from "styled-components";
 import { gql } from 'apollo-boost';
 import Loader from "../../components/Loader";
@@ -40,8 +41,34 @@ const View = styled.View`
   flex: 1;
 `;
 
+const Text = styled.Text`
+
+`;
+
 export default () => {
+  const [refreshing, setRefreshing] = useState(false);
   const { loading, data } = useQuery(FEED_QUERY);
+  const refresh = async () => {
+    try {
+      setRefreshing(true);
+      await refetch();
+    } catch(e) {
+      console.log(e);
+    } finally {
+      setRefreshing(false);
+    }
+  }
   console.log(loading, data);
-  return <View>{loading ? <Loader /> : null}</View>
+  return (
+    <ScrollView
+      refreshControl={
+        <RefreshControl
+          refreshing={refreshing}
+          onRefresh={refresh}
+        />
+      }
+    >
+      {loading ? <Loader /> : <Text>Hello</Text>}
+    </ScrollView>
+  );
 }
