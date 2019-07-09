213772354ae7c479d11ebdcdc2af70ab193d08ae
yongtany <yongn1494@gmail.com>
2019-07-09 22:59:05 +0900

    Preparing for Upload

--- apollo.js
+++ apollo.js
@@ -1,6 +1,6 @@
 const options = {
-    // uri: 'http://localhost:4000/'
-   uri:  "https://tanygram-backend.herokuapp.com"
+    uri: 'http://localhost:4000/'
+  //  uri:  "https://tanygram-backend.herokuapp.com"
 };
 
 export default options;
